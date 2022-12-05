e24357dbf82570e73a4e467199e18ce908388a66
tekostyshyn <tekostyshyn@gmail.com>
2022-12-05 12:46:16 +0200

    bug fixed

--- src/js/restaurant-modal.js
+++ src/js/restaurant-modal.js
@@ -2,14 +2,19 @@
   const refs = {
     openModalBtnTable: document.querySelector('[data-modal-table-open]'),
     closeModalBtnTable: document.querySelector('[data-modal-table-close]'),
+    openModalBtnTable2: document.querySelector('[data-modal-table-open-2]'),
+    closeModalBtnTable2: document.querySelector('[data-modal-table-close-2]'),
     modalTable: document.querySelector('[data-modal-table]'),
   };
 
   refs.openModalBtnTable.addEventListener('click', toggleModalTable);
   refs.closeModalBtnTable.addEventListener('click', toggleModalTable);
+  refs.openModalBtnTable2.addEventListener('click', toggleModalTable);
+  refs.closeModalBtnTable2.addEventListener('click', toggleModalTable);
+
 
   function toggleModalTable() {
     refs.modalTable.classList.toggle('is-hidden');
     document.body.classList.toggle('modal-open');
   }
-})();
+})(); 
